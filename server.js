7a19e3c8cc28bc6858d5011fdb4f73287dfc7757
Michael Vanderloon <jenkins92886@gmail.com>
2020-11-19 12:21:13 -0500

    push to heroku

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-// const path = require('path')
-
-
+const path = require('path')
 
 const app = express();
 
@@ -13,24 +11,22 @@ connectDB();
 app.use(express.json({extended: false}));
 
 
-
-
-// app.use('/movies', require('./routes/movies'));
-// app.use('/movie', require('./routes/movie'));
 app.use('/register', require('./routes/register'));
 app.use('/cart', require('./routes/cart'));
 app.use('/login', require('./routes/login'));
 app.use('/profile', require('./routes/profile'));
 app.use('/wishlist', require('./routes/wishlist'))
 
-// serve static assetes in production
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static('client/build'))
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*',(req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client' , 'build', 'index.html'))
+    })
+}
 
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
